fd3e0e0ac208b3fd95a0d710e3c94b003daaaa2d
chunliang yang <chunliangyang@chunliangdeMacBook-Pro.local>
2020-01-15 17:43:18 +0800

    change styles build

--- build/styles.build.js
+++ build/styles.build.js
@@ -6,20 +6,14 @@ const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 function compile() {
-  return src('../packages/styles/*.scss')
+  console.log(1)
+  return src('../packages/styles/index.scss')
     .pipe(sass.sync())
-    // .pipe(autoprefixer({
-    //   browsers: ['ie > 9', 'last 2 versions'],
-    //   cascade: false
-    // }))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
     .pipe(cssmin())
     .pipe(dest('../lib/styles'));
 }
-
-// function copyfont() {
-//   return src('./src/fonts/**')
-//     .pipe(cssmin())
-//     .pipe(dest('./lib/fonts'));
-// }
 
 exports.build = series(compile);
